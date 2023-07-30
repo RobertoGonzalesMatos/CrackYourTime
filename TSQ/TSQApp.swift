9781de72e2b0516c52b50f8fc624b29061de0db7
RobertoGonzalesMatos <roberto_gonzales_matos@brown.edu>
2023-07-30 17:38:39 -0400

    Initial Commit

---
+++ TSQ/TSQApp.swift
@@ -0,0 +1,17 @@
+//
+//  TSQApp.swift
+//  TSQ
+//
+//  Created by Roberto Gonzales on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct TSQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
